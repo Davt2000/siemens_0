b2e7d38724c46923d79282e5ea8db03aa99d5611
Davt2000 <davtart2000@yandex.ru>
2021-01-03 18:10:48 +0300

    created log_crawler.py

---
+++ log_crawler.py
@@ -0,0 +1,19 @@
+import os, sys
+
+if __name__ == '__main__':
+    # get wdir from argv
+    # assuming logs are organized like example ( logs/*category*/*category* )
+    # get listdir
+    # get listdir for every item in previous listdir in a list
+    # for item in a list
+    # check dirs in a test
+    # check missing files
+    # check *run* for errors and find an end
+    # compare files
+    #   memory check
+    #   *total* check
+    # write report
+    # echo in stdout
+
+    pass
+
